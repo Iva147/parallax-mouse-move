fcbb49aabc2c8b060cb5cd0d15510d485d206c68
Ivanna Liuba <iva147iva147@gmail.com>
2023-06-21 14:21:18 +0200

    expand perspective of card moving

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 document.addEventListener('mousemove', e => {
+	//.005 for --move-x for smother and smaller moving
 	Object.assign(document.documentElement, {
 		style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.01}deg; 
 		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
 		`
 	})
